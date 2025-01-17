fbf57940a1c72109cd2052c6f3b61e7879c51edf
Riccardo Santilli <riccardosantilli@live.it>
2025-01-17 16:18:00 +0100

    final release

--- src/main/java/it/epicode/W6_D5_test_Gestione_Eventi2/models/evento/Evento.java
+++ src/main/java/it/epicode/W6_D5_test_Gestione_Eventi2/models/evento/Evento.java
@@ -4,6 +4,7 @@ import it.epicode.W6_D5_test_Gestione_Eventi2.auth.AppUser;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Data;
 
@@ -17,19 +18,20 @@ public class Evento {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @NotBlank
+    @NotBlank(message = "Il titolo è obbligatorio")
     private String titolo;
 
-    @NotBlank
+    @NotBlank(message = "La descrizione è obbligatoria")
     private String descrizione;
 
     @FutureOrPresent
     private LocalDate data;
 
-    @NotBlank
+    @NotBlank(message = "Il luogo è obbligatorio")
     private String luogo;
 
     @Column(name = "posti_disponibili")
+    @NotNull
     private int postiDisponibili;
 
     @ManyToOne
